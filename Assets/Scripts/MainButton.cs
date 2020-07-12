80c68cebcfd2b3c5c172fffd04789e2c102cf2ce
Lance <ssummer.lance@gmail.com>
2020-07-11 23:14:06 -0700

    Working fade in button

--- Assets/Scripts/MainButton.cs
+++ Assets/Scripts/MainButton.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Text;
 
 public class MainButton : MonoBehaviour
 {
 	public API api;
 	public GameObject buttonGreen;
+    public GameObject buttonFadeInRed;
 	public Slider slider;
     public Text activePlaylistText;
     public string activePlaylistName;
     public string activePlaylistURI;
 	public int activePlaylistFirstTrackDuration;
+    public int volume;
     public bool isActive;
+    public bool fadeInButtonIsActive = false;
 
     public UserPlaylists_Item userPlaylistsItem;
 
@@ -31,9 +36,63 @@ public class MainButton : MonoBehaviour
 
 	public void OnPressed()
 	{
-		//Debug.Log("Start Playlist: " + activePlaylistName);
-		//Debug.Log("uri: " + activePlaylistURI);
+        if(fadeInButtonIsActive)
+        {
+            StartCoroutine(VolumeFadeIn());
+        }
 		API.instance.StartPlaylistRequest(activePlaylistURI, (int)slider.value);
-        //api.RequestCurrentPlaylistInfo();
+    }
+
+    public void FadeInButtonActivate()
+    {
+        if(!fadeInButtonIsActive)
+        {
+            fadeInButtonIsActive = true;
+            buttonFadeInRed.SetActive(true);
+        }
+        else
+        {
+            fadeInButtonIsActive = false;
+            buttonFadeInRed.SetActive(false);
+        }
+    }
+
+    IEnumerator VolumeFadeIn()
+    {
+        volume = 0;
+        while(volume < 101)
+        {
+            ChangeVolumeRequest();
+            yield return new WaitForSeconds(.03f);
+            volume++;
+            Debug.Log(volume);
+        }
+        volume = 100;
+    }
+
+    public void ChangeVolumeRequest()
+    {
+        string bodyJsonString = "Body: Info";
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        UnityWebRequest www = UnityWebRequest.Put("https://api.spotify.com/v1/me/player/volume?volume_percent=" + volume, bodyRaw);
+        www.SetRequestHeader("Content-Type", "application/json");
+        www.SetRequestHeader("Authorization", "Bearer " + API.instance.accessToken);
+
+        StartCoroutine(ChangeVolumeResponse(www));
+    }
+
+    IEnumerator ChangeVolumeResponse(UnityWebRequest www)
+    {
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
     }
 }
--- Assets/Scripts/SliderButton.cs
+++ Assets/Scripts/SliderButton.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class SliderButton : MonoBehaviour
 {
     public GameObject slider;
+    public GameObject buttonFadeIn;
     public bool isActive = false;
 
     public void Start()
     {
         slider.gameObject.SetActive(false);
+        buttonFadeIn.SetActive(false);
     }
 
     public void Update()
@@ -22,11 +24,13 @@ public class SliderButton : MonoBehaviour
         if(isActive == false)
         {
             slider.gameObject.SetActive(true);
+            buttonFadeIn.SetActive(true);
             isActive = true;
         }
         else
         {
             slider.gameObject.SetActive(false);
+            buttonFadeIn.SetActive(false);
             isActive = false;
         }
         
