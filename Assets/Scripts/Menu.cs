1338858dde6a3f41e47e0a1fdec5400cc43a4328
Lance <ssummer.lance@gmail.com>
2020-07-16 20:01:26 -0700

    Add menu and placeholder tutorial

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject tutorial;
+
+    public void OpenMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+
+    public void OpenTutorial()
+    {
+        tutorial.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorial.SetActive(false);
+    }
+}
